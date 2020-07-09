b48569f81e20563f844f118cc5e6ab777565844a
Everton Dias <esrdias@gmail.com>
2020-07-09 07:47:26 -0300

    Passando index para .php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php require('server/connect.php'); ?>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="css/bulma.min.0.9.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Hello, world!</title>
+</head>
+
+<body>
+	<!--<div class="painel">
+		<h1 class="title is-1">Bem-vindo a Star Store!</h1>
+		<h2 class="subtitle is-2">O Universo precisa ser explorado, sua viagem começa aqui</h2>
+	</div>-->
+
+	<div class="container">
+		<div class="columns is-multiline">
+			<div class="column is-one-third">
+				<div class="card">
+					<div class="card-image">
+						<figure class="image is-4by3">
+							<img src="images/ships/15942081735f05afadc5ca0.jpg" alt="Placeholder image">
+						</figure>
+					</div>
+					<div class="card-content">
+						<div class="content">
+							<h1 class="title">USS Enterprise NCC-1701</h1>
+							<h2 class="subtitle">Star Trek</h2>
+							Motor de dobra movido a matéria/anti-matéria e armada com Torpedos Phasers e Canhões de
+							Photon
+							<br><br>
+							<label>Capacidade <strong>6000</strong></label>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
