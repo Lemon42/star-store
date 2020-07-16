536c9851e2dcc45a17c5c81c8ac548bea71c8632
Everton Dias <esrdias@gmail.com>
2020-07-16 09:52:49 -0300

    Criando página de sobre

---
+++ pages/sobre.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="../css/bulma.min.0.9.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/default.css">
+
+	<title>Star Store - Sobre nós</title>
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+</head>
+
+<body>
+	<?php include('../model/navbar.html'); ?>
+
+	<div class="container" style="margin-top: 30px;">
+		<h1 class="title">Nosso time</h1>
+		<h2 class="subtitle">Elias Santos</h2>
+		Aluno da escola Etec Vasco Antônio Venchiarutti, que com muita dedicação se empenhou para ajudar no bem coletivo, e garantir o melhor para equipe. Com grande esforço obteve sucesso em seus ofícios escolares e com sua humildade beneficiou a todos ao seu redor.
+		<h2 class="subtitle">Everton Dias</h2>
+		Aluno da escola Etec Vasco Antônio Venchiarutti, onde se dedicou completamente a criação de sites e programas de forma exitosa e com muita determinação conseguiu alcançar os seus objetivos. Sempre se esforçou para dar o seu melhor em todos requisitos escolares, alcançando assim notoriedade em todos os seus projetos.
+		<h2 class="subtitle">Gabriel Lopes</h2>
+		Aluno da escola Etec Vasco Antônio Venchiarutti, que com muito esforço conseguiu, com mérito, se destacar em suas atividades escolares. Com seu carisma e simpatia conquistou a confiança de amigos e professores, tendo assim uma boa influência escolar.
+		<h2 class="subtitle">Igor Câmara</h2>
+		Aluno da escola Etec Vasco Antônio Venchiarutti, que com sua autoconfiança conseguiu de uma forma gratificante alcançar o ápice de seus objetivos e ganhar certo destaque em suas áreas de maior habilidade. Conquistando assim o respeito e apresso de professores e colegas.
+	</div>
+
+	<?php include('../model/footer.html'); ?>
+</body>
+</html>
