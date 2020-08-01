673ace8d0c394197efcc3890b5cca86358ffe599
Everton Dias <esrdias@gmail.com>
2020-08-01 12:01:18 -0300

    Mudanças de estilo no navbar e no texto de sobre

--- pages/sobre.php
+++ pages/sobre.php
@@ -9,22 +9,30 @@
 	<link rel="stylesheet" href="../css/default.css">
 
 	<title>Star Store - Sobre nós</title>
-	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+	<link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon" />
 </head>
 
 <body>
 	<?php include('../model/navbar.html'); ?>
 
-	<div class="container" style="margin-top: 30px;">
-		<h1 class="title">Nosso time</h1>
-		<h2 class="subtitle">Elias Santos</h2>
-		Aluno da escola Etec Vasco Antônio Venchiarutti, que com muita dedicação se empenhou para ajudar no bem coletivo, e garantir o melhor para equipe. Com grande esforço obteve sucesso em seus ofícios escolares e com sua humildade beneficiou a todos ao seu redor.
-		<h2 class="subtitle">Everton Dias</h2>
-		Aluno da escola Etec Vasco Antônio Venchiarutti, onde se dedicou completamente a criação de sites e programas de forma exitosa e com muita determinação conseguiu alcançar os seus objetivos. Sempre se esforçou para dar o seu melhor em todos requisitos escolares, alcançando assim notoriedade em todos os seus projetos.
-		<h2 class="subtitle">Gabriel Lopes</h2>
-		Aluno da escola Etec Vasco Antônio Venchiarutti, que com muito esforço conseguiu, com mérito, se destacar em suas atividades escolares. Com seu carisma e simpatia conquistou a confiança de amigos e professores, tendo assim uma boa influência escolar.
-		<h2 class="subtitle">Igor Câmara</h2>
-		Aluno da escola Etec Vasco Antônio Venchiarutti, que com sua autoconfiança conseguiu de uma forma gratificante alcançar o ápice de seus objetivos e ganhar certo destaque em suas áreas de maior habilidade. Conquistando assim o respeito e apresso de professores e colegas.
+	<div class="container is-fluid" style="margin-top: 30px;">
+		<div class="paragrafo">
+			<h1 class="title">Nosso time</h1>
+			<h2 class="subtitle">Elias Santos</h2>
+			Aluno da escola Etec Vasco Antônio Venchiarutti, que com muita dedicação se empenhou para ajudar no bem coletivo, e garantir o melhor para equipe. Com grande esforço obteve sucesso em seus ofícios escolares e com sua humildade beneficiou a todos ao seu redor.
+		</div>
+		<div class="paragrafo">
+			<h2 class="subtitle">Everton Dias</h2>
+			Aluno da escola Etec Vasco Antônio Venchiarutti, onde se dedicou completamente a criação de sites e programas de forma exitosa e com muita determinação conseguiu alcançar os seus objetivos. Sempre se esforçou para dar o seu melhor em todos requisitos escolares, alcançando assim notoriedade em todos os seus projetos.
+		</div>
+		<div class="paragrafo">
+			<h2 class="subtitle">Gabriel Lopes</h2>
+			Aluno da escola Etec Vasco Antônio Venchiarutti, que com muito esforço conseguiu, com mérito, se destacar em suas atividades escolares. Com seu carisma e simpatia conquistou a confiança de amigos e professores, tendo assim uma boa influência escolar.
+		</div>
+		<div class="paragrafo">
+			<h2 class="subtitle">Igor Câmara</h2>
+			Aluno da escola Etec Vasco Antônio Venchiarutti, que com sua autoconfiança conseguiu de uma forma gratificante alcançar o ápice de seus objetivos e ganhar certo destaque em suas áreas de maior habilidade. Conquistando assim o respeito e apresso de professores e colegas.
+		</div>
 	</div>
 
 	<?php include('../model/footer.html'); ?>
